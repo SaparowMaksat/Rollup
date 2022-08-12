54f307ecbde3d55bd6b9607fafb7dd5367c15d83
Max07 <102533562+SaparowMaksat@users.noreply.github.com>
2022-08-13 02:57:51 +0300

    Rollup Create

---
+++ Rollup/src/index.js
@@ -0,0 +1,9 @@
+import "../style.css";
+
+console.log('Maksat');
+
+const jsImageHtml = document.createElement('img');
+jsImageHtml.className = 'js-image';
+jsImageHtml.src = '../assets/01.jpeg';
+
+document.body.append(jsImageHtml);
